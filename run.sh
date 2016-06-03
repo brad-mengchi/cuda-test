1f586cc128c7dea563247f906803f2bb488aaf91
Mengchi Zhang <zhan2308@purdue.edu>
2016-06-02 23:52:58 -0400

    add cuda 4.0 result
    Signed-off-by: Mengchi Zhang <zhan2308@purdue.edu>

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
-nvcc -cudart shared -gencode arch=compute_35,code=sm_35 vectorAdd.cu -o vectorAdd
-./vectorAdd > run.log 2>&1
+nvcc -gencode arch=compute_10,code=sm_10 vectorAdd.cu -o vectorAdd
+cuobjdump -sass vectorAdd > res_sm10.log
+nvcc -gencode arch=compute_10,code=sm_13 vectorAdd.cu -o vectorAdd
+cuobjdump -sass vectorAdd > res_sm13.log
+nvcc -gencode arch=compute_20,code=sm_20 vectorAdd.cu -o vectorAdd
+cuobjdump -sass vectorAdd > res_sm20.log
+nvcc -gencode arch=compute_20,code=sm_23 vectorAdd.cu -o vectorAdd
+cuobjdump -sass vectorAdd > res_sm23.log
+#nvcc -cudart shared -gencode arch=compute_35,code=sm_35 vectorAdd.cu -o vectorAdd
